41c02f1a96bb1abc8ed3fe6876ee49bd49288d0d
Almarhoon Ibraheem <ibeem@outlook.com>
2023-07-29 18:43:29 +0300

    only load quicklisp module after ensuring it exists

--- .config/stumpwm/init.lisp
+++ .config/stumpwm/init.lisp
@@ -3,12 +3,14 @@
 (let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp"
                                        (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
-    (load quicklisp-init)))
+    (prog
+      (load quicklisp-init)
+      ;; setting font, needs a stumpwm module and quicklisp package
+      (ql:quickload :clx-truetype))))
+
 ;; (set-module-dir "/usr/share/stumpwm/contrib/")
 (add-to-load-path "~/.config/stumpwm/modules")
 
-;; setting font, needs a stumpwm module and quicklisp package
-(ql:quickload :clx-truetype)
 
 ;; load required modules if they exist
 (loop for module in '("globalwindows" "ttf-fonts" "swm-gaps")
